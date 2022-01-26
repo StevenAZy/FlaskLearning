bc16cb699749c06f6bfe31f1f4eab8a7953f711c
stevenazy <1315657491@qq.com>
2022-01-26 11:34:22 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -1,134 +1,27 @@
-import os
-import sys
-import click
-
-from flask import Flask, render_template, request, redirect, flash, url_for
-from flask_sqlalchemy import SQLAlchemy
-
-WIN = sys.platform.startswith('win')
-if WIN:
-    prefix = 'sqlite:///'
-else:
-    prefix = 'sqlite:////'
-
+from glob import escape
+from flask import Flask, url_for
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'dev'
-db = SQLAlchemy(app)
+
+@app.route('/')
+def hello():
+    return 'Welcome to My Watchlist'
+    
+
+@app.route('/home')
+def home():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(60))
-    year = db.Column(db.String(4))
-
-@app.cli.command()
-def initdb():
-    db.create_all()
-    click.echo('Initialized database')
-
-@app.cli.command()
-def forge():
-    db.create_all()
-    name = 'Steven'
-    movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
-    ]
-
-    user = User(name = name)
-    db.session.add(user)
-    for m in movies:
-        movie = Movie(title = m['title'], year = m['year'])
-        db.session.add(movie)
-
-    db.session.commit()
-    click.echo('Done')
+@app.route('/user/<name>')
+def user_page(name):
+    # return 'User Page'
+    return 'User : %s' % escape(name)
 
 
-@app.context_processor
-def inject_user():
-    user = User.query.first()
-    return dict(user = user)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        title = request.form.get('title')
-        year = request.form.get('year')
-        if not title or not year or len(year) > 4 or len(title) > 60:
-            flash('Invalid input.')
-            return redirect(url_for('index'))
-        movie = Movie(title=title, year=year)
-        db.session.add(movie)
-        db.session.commit()
-        flash('Item created.')
-        return redirect(url_for('index'))
-    # user = User.query.first()
-    movies = Movie.query.all()
-    # return render_template('index.html', user = user, movies = movies)
-    return render_template('index.html', movies = movies)
-
-@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
-def edit(movie_id):
-    movie = Movie.query.get_or_404(movie_id)
-
-    if request.method == 'POST':  # 处理编辑表单的提交请求
-        title = request.form['title']
-        year = request.form['year']
-
-        if not title or not year or len(year) != 4 or len(title) > 60:
-            flash('Invalid input.')
-            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
-
-        movie.title = title  # 更新标题
-        movie.year = year  # 更新年份
-        db.session.commit()  # 提交数据库会话
-        flash('Item updated.')
-        return redirect(url_for('index'))  # 重定向回主页
-
-    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
-
-
-@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
-def delete(movie_id):
-    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
-    db.session.delete(movie)  # 删除对应的记录
-    db.session.commit()  # 提交数据库会话
-    flash('Item deleted.')
-    return redirect(url_for('index'))  # 重定向回主页
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    # user = User.query.first()
-    # return render_template('404.html', user = user), 404
-    return render_template('404.html'), 404
-
-
-
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
-#
-#
-# @app.route('/user/<name>')
-# def user_page(name):
-#     return 'User:%s' % name
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/test')
+def test_url():
+    print(url_for('hello'))
+    print(url_for('user_page', name='stevenazy'))
+    print(url_for('test_url'))
+    print(url_for('test_url', num=2))
+    return 'Test Page'
